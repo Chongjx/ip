ee2b0fc00f06843ad002519d95fc831f7e4dace3
chongjx <chongjx60@gmail.com>
2020-08-25 23:16:04 +0800

    Added Echo and Exit command
    Echos commands entered by the user and exits when the user types bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,51 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Duke {
     // Formatting
     private static String dashOutline = "----------------------------------------------------------------------";
+    private static String indentation = "       ";
     private static String message = "";
     
     public static void reply(String msg) {
         System.out.println(msg + dashOutline);
     }
 
+    enum COMMAND {
+        INIT,
+        ADD,
+        LIST,
+        DONE,
+        EXIT,
+    }
+
+    private static boolean isExit = false;
+
     public static void main(String[] args) {
-        reply(message);
+        String userInput;
+        Scanner in = new Scanner(System.in);
+
+        COMMAND userCommand = COMMAND.INIT;
 
         // Welcome message
         message = "Hello! I'm Jay. Today is " + DateTimeManager.getDate() + ", " + DateTimeManager.getDay() + ". The " +
-                "time now is " + DateTimeManager.getTime() + "\nWhat can I do for you?\n";
+                "time now is " + DateTimeManager.getTime() + ".\nWhat can I do for you?\n";
         reply(message);
 
-        message = "Bye. Hope to see you again soon!\n";
+        do {
+            message = in.nextLine();
+            reply("");
+            
+            if (message.equalsIgnoreCase("BYE")) {
+                isExit = true;
+                break;
+            } else {
+                reply(indentation + message + "\n");
+            }
+        }
+        while (!isExit);
+
+        message = "Bye! Hope to see you again soon!\n";
         reply(message);
     }
 }
