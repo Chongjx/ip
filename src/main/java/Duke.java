d5d9cb4a3080b22e1f659f6d2737760d52ac9600
chongjx <chongjx60@gmail.com>
2020-08-28 18:59:59 +0800

    Refactored code to follow coding standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,21 +5,26 @@ import java.util.Scanner;
 
 public class Duke {
     // types of command
-    enum COMMAND {
-        INIT,
-        ADD,
-        LIST,
-        MARKDONE,
-        EXIT,
+    enum COMMANDS {
+        COMMAND_INIT,
+        COMMAND_ADD,
+        COMMAND_LIST,
+        COMMAND_MARKDONE,
+        COMMAND_EXIT,
     }
 
     // Formatting
-    private static String dashOutline = "----------------------------------------------------------------------";
-    private static String indentation = "       ";
+    private static String formatDashes = "----------------------------------------------------------------------";
+    private static String formatTwoTabs = "    ";
+    private static String formatFourTabs = "        ";
 
     // String object to hold any user input or output message
     private static String message = "";
     private static String command = "";
+    // Types of command that the user entered
+    private static COMMANDS userCommand = COMMANDS.COMMAND_INIT;
+
+    private static Scanner in = new Scanner(System.in);
 
     // Characters for tick and cross
     private static char tick = '\u2713';
@@ -33,7 +38,20 @@ public class Duke {
 
     // Print message with formatting
     private static void reply(String msg) {
-        System.out.println(msg + dashOutline);
+        System.out.println(msg + formatDashes);
+    }
+
+    private static void printWelcomeMessage() {
+        // Welcome message
+        message = formatTwoTabs + "Hello! I'm Jay. Today is " + DateTimeManager.getDate() + ", " + DateTimeManager.getDay() +
+                ". The " + "time now is " + DateTimeManager.getTime() + "." + System.lineSeparator() + formatTwoTabs + "What can I" +
+                " do for you?" + System.lineSeparator();
+        reply(message);
+    }
+
+    private static void printExitMessage() {
+        message = formatTwoTabs + "Bye! Hope to see you again soon!" + System.lineSeparator();
+        reply(message);
     }
 
     // Add task
@@ -43,7 +61,7 @@ public class Duke {
         // add to the list
         taskList.add(newTask);
         // print out the newly added task
-        reply(indentation + "Added task " + taskList.size() + ". " + message + System.lineSeparator());
+        reply(formatTwoTabs + "Added task " + taskList.size() + ". " + message + System.lineSeparator());
     }
 
     // List task
@@ -52,11 +70,11 @@ public class Duke {
         int index = 1;
         // print out default message if there are no task, else print the list of tasks
         if (taskList.size() == 0) {
-            output = indentation + "There are currently no task in the list! >.<" + System.lineSeparator();
+            output = formatTwoTabs + "There are currently no task in the list! >.<" + System.lineSeparator();
         } else {
-            output = indentation + "Here is your list of task(s):" + System.lineSeparator();
+            output = formatTwoTabs + "Here is your list of task(s):" + System.lineSeparator();
             for (Task i : taskList) {
-                output += indentation + index + ".[" + (i.getIsDone() ? tick : cross) + "]" + i.getDescription() + System.lineSeparator();
+                output += formatFourTabs + index + ".[" + (i.getIsDone() ? tick : cross) + "]" + i.getDescription() + System.lineSeparator();
                 ++index;
             }
         }
@@ -64,7 +82,7 @@ public class Duke {
     }
 
     // Mark tasks that are done
-    private static void MarkDone() {
+    private static void MarkTaskDone() {
         // Get the index of the space
         int spacingIndex = message.indexOf(" ");
         // Get the index that the user mark done
@@ -74,73 +92,63 @@ public class Duke {
         if (taskIndex > 0 && taskIndex <= taskList.size()) {
             Task task = taskList.get(taskIndex - 1);
             task.setIsDone(true);
-            message = indentation + "Completed task " + taskIndex + "!" + System.lineSeparator() + indentation + "[" +
+            message = formatTwoTabs + "Completed task " + taskIndex + "!" + System.lineSeparator() + formatFourTabs + "[" +
                     (task.getIsDone() ? tick : cross) + "]" + task.getDescription() + System.lineSeparator();
         } else {
-            message = indentation+ "Ops, you have entered an invalid task number! You have " + taskList.size() + " " +
-                    "task(s)!" + System.lineSeparator();
+            message = formatTwoTabs + "Ops, you have entered an invalid task number! You have " + taskList.size() +
+                    " task(s)!" + System.lineSeparator();
         }
         reply(message);
     }
 
+    private static void HandleUserInput() {
+        message = in.nextLine();
+        // reply for formatting
+        reply("");
+
+        command = (message.split(" "))[0];
+
+        // Set the type of user command based on input
+        switch (command.toUpperCase()) {
+        case "LIST":
+            userCommand = COMMANDS.COMMAND_LIST;
+            break;
+        case "DONE":
+            userCommand = COMMANDS.COMMAND_MARKDONE;
+            break;
+        case "BYE":
+            userCommand = COMMANDS.COMMAND_EXIT;
+            break;
+        default:
+            userCommand = COMMANDS.COMMAND_ADD;
+            break;
+        }
+
+        // Handles different user command
+        switch (userCommand) {
+        case COMMAND_INIT:
+            break;
+        case COMMAND_ADD:
+            AddTask();
+            break;
+        case COMMAND_LIST:
+            ListTask();
+            break;
+        case COMMAND_MARKDONE:
+            MarkTaskDone();
+            break;
+        case COMMAND_EXIT:
+            isExit = true;
+            break;
+        }
+    }
+
     public static void main(String[] args) {
-        // String to hold the user input
-        String userInput;
-        Scanner in = new Scanner(System.in);
-
-        // Types of command that the user entered
-        COMMAND userCommand = COMMAND.INIT;
-
-        // Welcome message
-        message = indentation + "Hello! I'm Jay. Today is " + DateTimeManager.getDate() + ", " + DateTimeManager.getDay() + ". The " +
-                "time now is " + DateTimeManager.getTime() + "." + System.lineSeparator() + indentation + "What can I" +
-                " do for you?" + System.lineSeparator();
-        reply(message);
-
+        printWelcomeMessage();
         do {
-            message = in.nextLine();
-            // reply for formatting
-            reply("");
-
-            command = (message.split(" "))[0];
-
-            // Set the type of user command based on input
-            switch (command.toUpperCase()) {
-            case "LIST":
-                userCommand = COMMAND.LIST;
-                break;
-            case "DONE":
-                userCommand = COMMAND.MARKDONE;
-                break;
-            case "BYE":
-                userCommand = COMMAND.EXIT;
-                break;
-            default:
-                userCommand = COMMAND.ADD;
-                break;
-            }
-
-            // Handles different user command
-            switch (userCommand) {
-            case INIT:
-                break;
-            case ADD:
-                AddTask();
-                break;
-            case LIST:
-                ListTask();
-                break;
-            case MARKDONE:
-                MarkDone();
-                break;
-            case EXIT:
-                isExit = true;
-                break;
-            }
+            HandleUserInput();
         }
         while (!isExit);
-
-        message = indentation + "Bye! Hope to see you again soon!" + System.lineSeparator();
-        reply(message);
+        printExitMessage();
     }
 }
