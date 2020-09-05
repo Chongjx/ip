e0e82da594f4267b037d9b220983b29953ae88f6
chongjx <chongjx60@gmail.com>
2020-09-05 21:18:20 +0800

    Remove unwanted package

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class DukeException {
+public class DukeException extends Exception {
+    public enum ExceptionType {
+        EXCEPTION_MISSING_DESCRIPTION("You missed out on the description?! O.o"),
+        EXCEPTION_MISSING_IDENTIFIER("You missed out key identifier! (/by /at)"),
+        EXCEPTION_MISSING_DATETIME("You did not set a date/time ¯\\_(ツ)_/¯");
+
+        private String exceptionMessage;
+
+        ExceptionType(String exceptionMessage) {
+            this.exceptionMessage = exceptionMessage;
+        }
+
+        @Override
+        public String toString() {
+            return exceptionMessage;
+        }
+    }
+
+    public DukeException(ExceptionType exception) {
+        super(exception.toString());
+    }
 }
